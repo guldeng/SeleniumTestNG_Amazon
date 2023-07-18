d9b4d6b2da620b405c2df35687f9d870ab13343d
gozukara <gulden205@yahoo.com>
2023-07-18 10:30:33 +0200

    first commit

---
+++ src/test/java/com/amazon/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.amazon.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage {
+
+    @FindBy(id ="#sp-cc-accept")
+    public WebElement cookies;
+}
